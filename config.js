0f453b25890465a5b20da674faf6564b534001d4
Richard Fenton <89595400+richfenton@users.noreply.github.com>
2024-04-23 11:46:53 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,95 @@
+let Styles = {
+  widgetContainer: {
+    styles: {
+      width: '370px',
+      height: '530px',
+      right: '50px',
+      bottom: '90px',
+    }
+  },
+  headerProps: {
+    icon: 'https://zincgroupdevelopment.com/wp-content/uploads/2020/06/Zinc-Group-Ltd-Logo-P2935.png',
+    title: 'Chat with ZAC',
+    backgroundColor: '#234165',
+    logoProps: {
+      width: '40px',
+      height: '40px'
+    },
+    titleProps: {
+      color: '#fff',
+      fontSize: '18px',
+      fontFamily: 'inherit',
+    },
+    borderRadius: {
+      topRight: '12px',
+      topLeft: '12px',
+      bottomRight: '0px',
+      bottomLeft: '0px',
+    }
+  },
+  bodyProps: {
+    botLogoProps: {
+      icon: 'https://cdn-icons-png.flaticon.com/512/4712/4712109.png',
+      width: '32px',
+      height: '32px',
+    },
+    inboundMessageProps: {
+      backgroundColor: '#f4f4f4',
+      fontSize: '15px',
+      borderRadius: '6px',
+      color: '#4b4b4b',
+      maxWidth: '267px',
+      inboundTimeProps: {
+        timeAlignSelf: 'start',
+        color: '#4b4b4b'
+      }
+    },
+    outboundMessageProps: {
+      backgroundColor: '#FFEDE9',
+      borderRadius: '6px',
+      maxWidth: '262px',
+      color: '#4B4B4B',
+      fontSize: '16px',
+      outboundTimeProps: {
+        timeAlignSelf: 'end',
+        color: '#4b4b4b'
+      }
+    },
+  },
+  footerProps: {
+    inputStyle: {
+      width: '80%',
+      height: '',
+      borderRadius: '60px',
+      borderColor: '#e9e9e9',
+      borderSize: '1px',
+      color: '#4b4b4b',
+      placeholderColor: '#78787c',
+    },
+    buttonSendProps: {
+      width: '50px',
+      height: '50px',
+      backgroundColor: '#35618d',
+    },
+    buttonImageProps: {
+      icon: 'https://cdn.pixabay.com/photo/2015/12/07/22/53/paper-planes-1081560_1280.png',
+      width: '18px',
+      height: '18px',
+    }
+  },
+  openButtonProps: {
+    buttonImage: 'https://zincgroupdevelopment.com/wp-content/uploads/2024/04/Chat-Button-Two.png',
+    buttonProps: {
+      width: '80px',
+      height: '80px',
+      borderSize: '3px',
+      borderColor: '#124867',
+      textColor: '#fff',
+      backgroundColor: '#124867',
+    },
+    buttonImageProps: {
+      width: '40px',
+      heigth: '40px',
+    }
+  }
+};
